e21acde0b4b1a7428f57ac341559000edfed0eff
Maxime Augier <max@xolus.net>
2008-08-25 21:17:56 +0200

    ANSI color test works

---
+++ greeter.erl
@@ -0,0 +1,15 @@
+-module(greeter).
+-export([answer/1, run/0]).
+
+
+message() -> format:parse(["Hello ", {color, red, "red"}, " ",
+				{color, blue, "blue"}]).
+
+
+answer(Socket) ->
+	gen_tcp:send(Socket, message()),
+	ok.
+
+
+run() ->
+	listener:start_link(1234, fun greeter:answer/1).
