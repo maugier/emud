5f894c893abe359c7cf46d7998b022212677ea80
Maxime Augier <max@xolus.net>
2009-01-28 21:27:25 +0100

    Character can be activated and shut down

--- character.erl
+++ character.erl
@@ -5,7 +5,8 @@
 
 -include("game.hrl").
 
--export([start_link/1, init/1, handle_call/3, terminate/2]).
+-export([start_link/1, init/1, handle_call/3, handle_cast/2, 
+handle_info/2, terminate/2]).
 
 
 start_link(Name) ->
@@ -13,12 +14,23 @@ start_link(Name) ->
 	gen_server:start_link({global, {character, Name}}, 
 		?MODULE, {Name,Ctrl}, []).
 	
-init({Name,_Ctrl}) ->
-	{ok, _Char} = char_db:load(Name).
+init({Name,Ctrl}) ->
+	log:msg('DEBUG', "Character server [~s] starting", [Name]),
+	{ok, Char} = char_db:load(Name),
+	{ok, {Char, Ctrl}}.
 
-handle_call({get,name}, _From, Char) -> {ok, Char#character.name, Char}.
+handle_call({get,name}, _From, {Char,_}) ->
+	{ok, Char#character.name, Char}.
 
-terminate(_Reason, Char) ->
+handle_cast(shutdown,S) -> 
+	{stop, shutdown, S}.
+
+handle_info(_I,S) ->
+	{noreply, S}.
+
+terminate(Reason, {Char,_}) ->
+	log:msg('DEBUG', "Character server [~s] terminating: ~s",
+		[Char#character.name, Reason]),
 	char_db:save(Char).
 
 
